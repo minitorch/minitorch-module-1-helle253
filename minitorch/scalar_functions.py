8ad171888fd083432822f914a3c7efa0ffa29dfb
helle253 <nathanhellbhoy@gmail.com>
2024-09-18 09:50:29 -0500

    implement Exp backwards fn

--- minitorch/scalar_functions.py
+++ minitorch/scalar_functions.py
@@ -171,12 +171,14 @@ class Exp(ScalarFunction):
 
     @staticmethod
     def forward(ctx: Context, a: float) -> float:
+        ctx.save_for_backward(a)
         return operators.exp(a)
 
     @staticmethod
     def backward(ctx: Context, d_output: float) -> float:
-        # TODO: Implement for Task 1.4.
-        raise NotImplementedError("Need to implement for Task 1.4")
+        (a,) = ctx.saved_values
+
+        return operators.mul(d_output, operators.exp(a))
 
 
 class LT(ScalarFunction):
